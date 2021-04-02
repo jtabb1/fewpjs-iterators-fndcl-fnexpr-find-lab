5b332b7d656967248351a3ec9563d4814371edcc
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-04-02 01:44:19 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+const superbowlWin = (a) => {
+    if(a.find( o => o.result === "W")) {
+        return a.find( o => o.result === "W").year;
+    } else {
+        return a.find( o => o.result === "W");
+    }
+}
